3f14f1e38c5742cd26bacb2afd4870ed0f0966b7
Rico <ricochen05@gmail.com>
2025-08-14 13:39:20 +0800

    feat: 去除各种垃圾+精简不必要的功能

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -4,14 +4,14 @@
       <el-col>
         <el-card>
           <div slot="header">
-            <svg-icon class="gayhub" icon-class="github" style="float:left" @click="goToProject"/>
-            <svg-icon class="dianbao" icon-class="telegram" style="float:left;margin-left: 10px"
-                      @click="gotoTgChannel"/>
-            <svg-icon class="bilibili" icon-class="bilibili" style="float:right;margin-left:10px"
-                      @click="gotoBiliBili"/>
-            <svg-icon class="youguan" icon-class="youtube" style="float:right;margin-left:10px" @click="gotoYouTuBe"/>
-            <svg-icon class="channel" icon-class="telegram" style="float:right;margin-left: 10px"
-                      @click="gotoTgChannel"/>
+<!--            <svg-icon class="gayhub" icon-class="github" style="float:left" @click="goToProject"/>-->
+<!--            <svg-icon class="dianbao" icon-class="telegram" style="float:left;margin-left: 10px"-->
+<!--                      @click="gotoTgChannel"/>-->
+<!--            <svg-icon class="bilibili" icon-class="bilibili" style="float:right;margin-left:10px"-->
+<!--                      @click="gotoBiliBili"/>-->
+<!--            <svg-icon class="youguan" icon-class="youtube" style="float:right;margin-left:10px" @click="gotoYouTuBe"/>-->
+<!--            <svg-icon class="channel" icon-class="telegram" style="float:right;margin-left: 10px"-->
+<!--                      @click="gotoTgChannel"/>-->
             <div style="text-align:center;font-size:15px">订 阅 转 换</div>
           </div>
           <el-container>
@@ -41,17 +41,17 @@
                   <el-option v-for="(v, k) in options.customBackend" :key="k" :label="k" :value="v"></el-option>
                 </el-select>
               </el-form-item>
-              <el-form-item label="短链选择:">
-                <el-select
-                    v-model="form.shortType"
-                    allow-create
-                    filterable
-                    placeholder="可输入其他可用短链API"
-                    style="width: 100%"
-                >
-                  <el-option v-for="(v, k) in options.shortTypes" :key="k" :label="k" :value="v"></el-option>
-                </el-select>
-              </el-form-item>
+<!--              <el-form-item label="短链选择:">-->
+<!--                <el-select-->
+<!--                    v-model="form.shortType"-->
+<!--                    allow-create-->
+<!--                    filterable-->
+<!--                    placeholder="可输入其他可用短链API"-->
+<!--                    style="width: 100%"-->
+<!--                >-->
+<!--                  <el-option v-for="(v, k) in options.shortTypes" :key="k" :label="k" :value="v"></el-option>-->
+<!--                </el-select>-->
+<!--              </el-form-item>-->
               <el-form-item label="远程配置:">
                 <el-select
                     v-model="form.remoteConfig"
@@ -184,12 +184,12 @@
               </el-form-item>
               <div style="margin-top: 30px"></div>
               <el-divider content-position="center">
-                <el-button
-                    type="zhuti"
-                    @click="change">
-                  <i id="rijian" class="el-icon-sunny"></i>
-                  <i id="yejian" class="el-icon-moon"></i>
-                </el-button>
+<!--                <el-button-->
+<!--                    type="zhuti"-->
+<!--                    @click="change">-->
+<!--                  <i id="rijian" class="el-icon-sunny"></i>-->
+<!--                  <i id="yejian" class="el-icon-moon"></i>-->
+<!--                </el-button>-->
               </el-divider>
               <el-form-item label="定制订阅:">
                 <el-input class="copy-content" disabled v-model="customSubUrl">
@@ -203,19 +203,19 @@
                   </el-button>
                 </el-input>
               </el-form-item>
-              <el-form-item label="订阅短链:">
-                <el-input class="copy-content" v-model="customShortSubUrl"
-                          placeholder="输入自定义短链接后缀，点击生成短链接可反复生成">
-                  <el-button
-                      slot="append"
-                      v-clipboard:copy="customShortSubUrl"
-                      v-clipboard:success="onCopy"
-                      ref="copy-btn"
-                      icon="el-icon-document-copy"
-                  >复制
-                  </el-button>
-                </el-input>
-              </el-form-item>
+<!--              <el-form-item label="订阅短链:">-->
+<!--                <el-input class="copy-content" v-model="customShortSubUrl"-->
+<!--                          placeholder="输入自定义短链接后缀，点击生成短链接可反复生成">-->
+<!--                  <el-button-->
+<!--                      slot="append"-->
+<!--                      v-clipboard:copy="customShortSubUrl"-->
+<!--                      v-clipboard:success="onCopy"-->
+<!--                      ref="copy-btn"-->
+<!--                      icon="el-icon-document-copy"-->
+<!--                  >复制-->
+<!--                  </el-button>-->
+<!--                </el-input>-->
+<!--              </el-form-item>-->
               <el-form-item label-width="0px" style="margin-top: 40px; text-align: center">
                 <el-button
                     style="width: 120px"
@@ -224,14 +224,14 @@
                     :disabled="form.sourceSubUrl.length === 0 || btnBoolean"
                 >生成订阅链接
                 </el-button>
-                <el-button
-                    style="width: 120px"
-                    type="danger"
-                    @click="makeShortUrl"
-                    :loading="loading1"
-                    :disabled="customSubUrl.length === 0"
-                >生成短链接
-                </el-button>
+<!--                <el-button-->
+<!--                    style="width: 120px"-->
+<!--                    type="danger"-->
+<!--                    @click="makeShortUrl"-->
+<!--                    :loading="loading1"-->
+<!--                    :disabled="customSubUrl.length === 0"-->
+<!--                >生成短链接-->
+<!--                </el-button>-->
               </el-form-item>
               <el-form-item label-width="0px" style="text-align: center">
                 <el-button
@@ -251,15 +251,15 @@
                 >从URL解析
                 </el-button>
               </el-form-item>
-              <el-form-item label-width="0px" style="text-align: center">
-                <el-button
-                    style="width: 250px;"
-                    type="success"
-                    icon="el-icon-video-play"
-                    @click="centerDialogVisible = true"
-                >视频教程
-                </el-button>
-              </el-form-item>
+<!--              <el-form-item label-width="0px" style="text-align: center">-->
+<!--                <el-button-->
+<!--                    style="width: 250px;"-->
+<!--                    type="success"-->
+<!--                    icon="el-icon-video-play"-->
+<!--                    @click="centerDialogVisible = true"-->
+<!--                >视频教程-->
+<!--                </el-button>-->
+<!--              </el-form-item>-->
             </el-form>
           </el-container>
         </el-card>
@@ -474,13 +474,14 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
-          "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
-          "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
-          nameless13提供: "https://www.nameless13.com",
-          subconverter作者提供: "https://sub.xeton.dev",
-          "sub-web作者提供": "https://api.wcc.best",
-          "sub作者&lhie1提供": "https://api.dler.io",
+          "夜猫人": "https://subconverter.home.yemaoren.net"
+          // "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
+          // "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
+          // "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
+          // nameless13提供: "https://www.nameless13.com",
+          // subconverter作者提供: "https://sub.xeton.dev",
+          // "sub-web作者提供": "https://api.wcc.best",
+          // "sub作者&lhie1提供": "https://api.dler.io",
         },
         backendOptions: [
           {value: "https://url.v1.mk"},
@@ -889,7 +890,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://url.v1.mk" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://subconverter.home.yemaoren.net" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
         remoteConfig: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_NoAuto.ini",
         excludeRemarks: "",
@@ -897,7 +898,7 @@ export default {
         filename: "",
         rename: "",
         devid: "",
-        interval: "",
+        interval: 1,
         emoji: true,
         nodeList: false,
         extraset: false,
@@ -946,10 +947,9 @@ export default {
     this.isPC = this.$getOS().isPc;
   },
   mounted() {
-    this.tanchuang();
     this.form.clientType = "clash";
     this.getBackendVersion();
-    this.anhei();
+    // this.anhei();
     let lightMedia = window.matchMedia('(prefers-color-scheme: light)');
     let darkMedia = window.matchMedia('(prefers-color-scheme: dark)');
     let callback = (e) => {
@@ -1008,13 +1008,6 @@ export default {
         document.getElementsByTagName('body')[0].setAttribute('class', 'light-mode');
         window.localStorage.setItem('localTheme', 'light-mode');
       }
-    },
-    tanchuang() {
-      this.$alert(`<div style="text-align:center;font-size:15px"><strong><span style="font-size:20px;color:red">apiurl.v1.mk已被蔷，请更换最新的url.v1.mk</span></strong></br><strong><span style="font-size:20px">本站官方TG交流群：</span><span><a href="https://t.me/feiyangdigital" target="_blank" style="color:red;font-size:20px;text-decoration:none">点击加入</a></span></strong></br><strong><span style="font-size:20px">IEPL高端机场（<span style="color:blue">原生支持奈飞非自制剧、Disney Plus、HBO等各种流媒体，支持Chat-GPT和ISP住宅IP助力Tiktok等跨境贸易使用</span>）：</span><span><a href="https://www.mcwy.org" style="color:red;font-size:20px;text-decoration:none">点击注册</a></span></strong></br><strong><span style="font-size:20px">奈飞、ChatGPT合租（<span style="color:blue">优惠码：feiyang</span>）：</span><span><a href="https://hezu.v1.mk/" style="color:red;font-size:20px;text-decoration:none">点击上车</a></span></strong></br><strong><span style="font-size:20px">115蓝光4K原盘内部资源群：</span><span><a href="https://readme.115vip.shop/" target="_blank" style="color:red;font-size:20px;text-decoration:none">点击查看</a></span></strong></br>本站服务器赞助机场-牧场物语，是一家拥有BGP中继+IEPL企业级内网专线的高端机场，适合各个价位要求的用户，牧场物语采用最新的奈飞非自制剧解决方案，出口随机更换IP，确保尽可能的每个用户可以用上独立IP，以此来稳定解决奈飞非自制剧的封锁，并推出7*24小时奈飞非自制剧节点自动检测系统，用户再也不用自己手动一个个的乱试节点了，目前牧场的新加坡，台湾等节区域点均可做到24H稳定非自制剧观看，支持Chat-GPT和ISP住宅IP助力Tiktok等跨境贸易使用！</br></div>`, '信息面板', {
-        confirmButtonText: '确定',
-        dangerouslyUseHTMLString: true,
-        customClass: 'msgbox'
-      });
     },
     onCopy() {
       this.$message.success("已复制");
@@ -1402,9 +1395,8 @@ export default {
           .then(res => {
             this.backendVersion = res.data.replace(/backend\n$/gm, "");
             this.backendVersion = this.backendVersion.replace("subconverter", "SubConverter");
-            let a = this.form.customBackend.indexOf("url.v1.mk") !== -1 || this.form.customBackend.indexOf("sub.d1.mk") !== -1;
             let b = this.form.customBackend.indexOf("127.0.0.1") !== -1;
-            a ? this.$message.success(`${this.backendVersion}` + "肥羊负载均衡增强版后端，已屏蔽免费节点池（会返回403），额外支持vless reality+hysteria+hysteria2订阅转换") : b ? this.$message.success(`${this.backendVersion}` + "本地局域网自建版后端") : this.$message.success(`${this.backendVersion}` + "官方原版后端不支持vless/hysteria订阅转换");
+            b ? this.$message.success(`${this.backendVersion}` + "本地局域网自建版后端") : this.$message.success(`${this.backendVersion}` + "官方原版后端不支持vless/hysteria订阅转换");
           })
           .catch(() => {
             this.$message.error("请求SubConverter版本号返回数据失败，该后端不可用！");
